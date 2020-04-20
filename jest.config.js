cf474887f4d1a89da598af74f1e840ea70502c82
Maianne Ribeiro <mai628r@gmail.com>
2020-04-20 18:38:30 -0300

    fix(rts): update deprecated jest config

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   globals: {
     'ts-jest': {
-      tsConfig: '<rootDir>/tsconfig.json'
-    }
+      tsConfig: '<rootDir>/tsconfig.json',
+    },
   },
   roots: ['<rootDir>/src'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
+    '^.+\\.tsx?$': 'ts-jest',
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleDirectories: ['node_modules', 'src'],
@@ -15,7 +15,7 @@ module.exports = {
     'components/(.*)': '<rootDir>/src/components/$1',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
-    '\\.(css|less|scss)$': 'identity-obj-proxy'
+    '\\.(css|less|scss)$': 'identity-obj-proxy',
   },
-  setupTestFrameworkScriptFile: '<rootDir>/__mocks__/setupEnzyme.js'
+  setupFilesAfterEnv: ['<rootDir>/__mocks__/setupEnzyme.js'],
 };
